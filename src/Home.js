7ac85375112e1b645099717def758ec00429fd5a
Tatpol Chutipat <tatpolchutipat@gmail.com>
2023-09-07 13:18:12 +0700

    Edit homepage, add url to name in navbar

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return ( 
         <div className="home">
-            <h1>Tatpol Chutipat (Ohm)</h1>
-            <p>This is suppose to be my website introducing myself so welcome...I guess</p>
+            <h1>Thank you for visiting my website!</h1>
+            <p>Please feel free to take a look into my website and it's content.</p>
+            <p>If you would like to see which programming language I know, please see in the <Link className="home-link" to="/skills">skills</Link> page.</p>
+            <p>My active and past project will be at the <Link className="home-link" to="/projects">projects</Link> page.</p>
+            <p>If you would love to learn more about me, please see the <Link className="home-link" to="/about">about me</Link> page.</p>
+            <p>And lastly, if you would like to contact or follow me, it will be in the <Link className="home-link" to="/contact">contact</Link> page.</p>
         </div>
      );
 }
